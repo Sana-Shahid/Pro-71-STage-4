e29b5611a01bcbed1c856bb5cf587ae87da93703
Sana-Shahid <77516425+Sana-Shahid@users.noreply.github.com>
2022-09-05 08:56:22 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+//Paste your firebaseConfig here
+const firebaseConfig = {
+    apiKey: "AIzaSyDqJ_jv9ujwYjJBwn2Zj2vxpiNduEGJDdg",
+    authDomain: "e-ride-stage-4-7c620.firebaseapp.com",
+    projectId: "e-ride-stage-4-7c620",
+    storageBucket: "e-ride-stage-4-7c620.appspot.com",
+    messagingSenderId: "357197295048",
+    appId: "1:357197295048:web:f61cdbe7913d6c0c8fa4fd"
+  };
+
+//
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
